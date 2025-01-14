4ccc85fa9536e39f533209a6913a6522d74c19be
harsh <hg3172003@gmail.com>
2025-01-14 12:50:18 +0530

    up-1

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../context";
 import { motion } from "framer-motion";
-import Resume from './resume.pdf';
+import Resume from './resume_xy.pdf';
 
 const Services = () => {
   // context
